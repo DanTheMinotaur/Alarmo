ffa5ebd01f1b196b1b9ba9c9334c4440c997196d
Daniel <x15004091@student.ncirl.ie>
2019-04-03 13:00:45 +0100

    Alarm Setter Dialog

--- AndroidApp/app/src/main/java/online/danshub/alarmo/alarmocomanion/MainActivity.java
+++ AndroidApp/app/src/main/java/online/danshub/alarmo/alarmocomanion/MainActivity.java
@@ -1,16 +1,22 @@
 package online.danshub.alarmo.alarmocomanion;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TimePickerDialog timePickerDialog;
+    private String LOGTAG = "MainMenu";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +24,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        createDialogs();
+        createButtons();
+
+    }
+
+    private void createDialogs() {
+        timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                Log.v(LOGTAG, "Hour: " + hourOfDay + " - Minute: " + minutes);
+            }
+        }, 0, 0,  true);
+    }
+
+    private void createButtons() {
+        FloatingActionButton alarmChooserButton = findViewById(R.id.alarmCreateButton);
+        alarmChooserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog.show();
             }
         });
     }
